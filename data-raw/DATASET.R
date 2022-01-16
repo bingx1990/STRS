f8c91bfde18354e6ec4f02f85491b390c893e17b
Xin Bing <bingx1990@hotmail.com>
2022-01-16 15:43:50 -0500

    Merge code for generating synthetic data.

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,3 +1,21 @@
-## code to prepare `DATASET` dataset goes here
+################################################################################
+######                                                                   #######
+######                    Code for generating example data               #######
+######                                                                   #######
+################################################################################
 
-usethis::use_data(DATASET, overwrite = TRUE)
+source("/Users/mike/Documents/Mike/Projects/Reduced Rank/Simulation/new_code/Gen_data.R")
+library(MASS)
+
+
+rho <- 0.1; b0 <- 0.2;
+n <- 50
+p <- q <- m <- 20
+r <- 3
+
+X = getX(rho, n, p, q, high = F)
+A = getA(b0, p, r, m)
+Y = X %*% A + getE(n, m)
+
+usethis::use_data(X)
+usethis::use_data(Y)
